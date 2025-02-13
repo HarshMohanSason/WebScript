10e1850715e9e0ea8b89d1a2f81dc39367e8cc5a
harshmohansason <harshsason2000@gmail.com>
2025-02-13 05:24:05 -0800

    New changes

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,24 @@ import Lexer (tokenize)
 
 main :: IO ()
 main = do
-    let filePath = "/Users/harshmohansason/Documents/WebScript/assets/sample.txt"
-    content <- readFile filePath
-    print (tokenize content)
+    let filePath = "/Users/harshmohansason/Documents/WebScript/assets/sample.wsc"
+    case checkFileFormat filePath of
+        Right msg -> do 
+            putStrLn msg
+            content <- readFile filePath
+            print (tokenize content)
+        Left err -> putStrLn err
 
+{- 
+   Reverses the file path, extracts the extension, then checks if it's "wsc".
+   Example: "../filename.wsc"
+   1. Reverse path: "csw.emanelif"
+   2. Extract until dot: "csw"
+   3. Reverse back: "wsc"
+-}
+checkFileFormat :: String -> Either String String
+checkFileFormat filePath = do
+    let ext = reverse $ takeWhile (/= '.') (reverse filePath)
+    if ext == "wsc"
+        then Right "Running..."
+        else Left "Error! wrong file format. Make sure the file format ends with a 'wsc' extension "
--- src/Lexer.hs
+++ src/Lexer.hs
@@ -8,12 +8,11 @@ tokenize :: String -> [Token]
 tokenize [] = []
 tokenize (c:cs) 
     | isSpace c = tokenize cs  -- Ignore any spaces 
-    | isAlpha c = let (word, rest) = span isAlpha (c:cs) -- 
+   -- | isAlpha c = let (word, rest) = span isAlpha (c:cs) -- 
     | c == '='  = Equals : tokenize cs -- Match the '=' sign 
     | c == '+'  = Addition : tokenize cs -- Match the '+' sign
     | c == '-'  = Subtraction : tokenize cs -- Match the '-' sign
     | c == '/'  = Division : tokenize cs -- Match the '/' sign
     | c == '*'  = Multiplication : tokenize cs -- Match the '*' sign
-    | 
     | otherwise = InvalidInput [c] : tokenize cs -- Temporary default case 
 
